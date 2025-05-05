57eaf084dc318a7f416a0ac23ec6e5e1b22871c8
Wendrew Oliveira <wendrewdevelop@gmail.com>
2025-05-05 14:28:37 -0300

    update install.sh

--- install.sh
+++ install.sh
@@ -5,13 +5,14 @@ set -e
 if ! command -v pipx &> /dev/null; then
   echo "➡️ pipx não encontrado. Instalando pipx..."
   python3 -m pip install --user pipx
+  sudo apt install -y python3-pip pipx
   python3 -m pipx ensurepath
   export PATH="$HOME/.local/bin:$PATH"
 fi
 
 # 2. Define a versão do dee
 REPO="wendrewdevelop/dee"
-VERSION="${1:-v0.1.0}"
+VERSION="${1:-v0.1.2}"
 TARBALL_URL="https://github.com/${REPO}/archive/refs/tags/${VERSION}.tar.gz"
 
 # 3. Instala o dee via pipx
