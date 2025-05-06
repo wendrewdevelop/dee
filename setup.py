e13a79a449dfbb5e3eee4b20d14289da534ece4f
Wendrew Oliveira <wendrewdevelop@gmail.com>
2025-05-06 17:38:28 -0300

    fix build problems

--- setup.py
+++ setup.py
@@ -2,23 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="dee",
-    version="0.1.11",
-    packages=find_packages(where="src"),    # encontra só dentro de src/
-    package_dir={"": "src"},                # define que src/ é a raiz dos pacotes
-    install_requires=[
-        "click",
-        "django",
-        "djangorestframework",
-        "jinja2",
-        "paramiko",
-        "psycopg2-binary",
-        "pygments",
-        "python-decouple",
-        "requests",
-    ],
+    version="0.1.12",
+    packages=find_packages(),
     entry_points={
         "console_scripts": [
-            "dee=cli.commands:cli",           
+            "dee=src.main:main",           
         ],
     },
 )
