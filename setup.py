5fa1d7be33a8441a6ddf38560a005909839da62a
Wendrew Oliveira <wendrewdevelop@gmail.com>
2025-05-06 16:45:18 -0300

    adjusting setup.py and main.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dee",
-    version="0.1.6",
+    version="0.1.9",
     packages=find_packages(),
     install_requires=[],
     entry_points={
--- src/main.py
+++ src/main.py
@@ -1,2 +1,10 @@
+from .cli.commands import cli
+
+
 def main():
     print("Bem-vindo ao dee!")
+    cli()
+
+
+if __name__ == "__main__":
+    main()
